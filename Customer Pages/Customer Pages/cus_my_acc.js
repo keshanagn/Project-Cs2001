e641e2f7c68ad75ebf680d9e187a7a4816ab8eff
keshanagn <144969638+keshanagn@users.noreply.github.com>
2023-10-26 14:00:46 +0530

    Add files via upload
    Customer Page Prototype 1 files

---
+++ Customer Pages/Customer Pages/cus_my_acc.js
@@ -0,0 +1,19 @@
+var myiframe=document.getElementById("frame_1");
+myiframe.src="./cus_my_acc.html";
+
+function chnge_frame_1(){
+    document.getElementById("frame_1").src="./cus_my_acc.html";
+}
+
+function chnge_frame_2(){
+    document.getElementById("frame_1").src="./cus_feedback.html";
+}
+
+function chnge_frame_3(){
+    document.getElementById("frame_1").src="./cus_mycontacts.html";
+}
+
+function chnge_frame_4(){
+    document.getElementById("frame_1").src="./cus_tasks.html";
+}
+
